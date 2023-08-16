5cb9459d339eea560e409628fd22bff76cc201f7
lymei <lymeicontato@gmail.com>
2023-08-16 20:21:02 -0300

    feat: Finalized Rotation

--- src/components/DimensionalCard.tsx
+++ src/components/DimensionalCard.tsx
@@ -13,6 +13,8 @@ export default function DimensionalCard() {
 	const maxDegreeYarn = 10;
 	const halfDegreeYarn = 5;
 
+	const heightSide = yPercentage * 2 - 1;
+
 	/* rotate3d(0,1,0,${xPercentage * maxDegree - maxDegree / 2}deg) rotate3d(-1,0,0,${
 					yPercentage * maxDegree - maxDegree / 2
 				}deg)  */
@@ -21,14 +23,11 @@ export default function DimensionalCard() {
 		<div
 			className='w-[50vw] h-[50vh] flex flex-col justify-between p-16 border border-neutral-800 bg-gradient-to-br from-purple-500 via-blue-500 to-orange-500 shadow-lg shadow-white/20 rounded-xl transition-transform duration-75'
 			style={{
-				transform: `rotate3d(0,0,1,${
-					xPercentage * (yPercentage >= 0.5 ? 1 : -1) * maxDegreeYarn - halfDegreeYarn
-				}deg)`,
+				transform: `rotate3d(0,1,0,${xPercentage * maxDegree - halfDegree}deg) rotate3d(-1,0,0,${
+					yPercentage * maxDegree - halfDegree
+				}deg) rotate3d(0,0,${heightSide},${xPercentage * maxDegreeYarn - halfDegreeYarn}deg)`,
 			}}>
 			<p className='text-right text-4xl font-bold'>HIGH BANK</p>
-			<p className='absolute left-4 top-4'>
-				{xPercentage.toFixed(2)} : {yPercentage.toFixed(2)} => {xPercentage * (yPercentage >= 0.5 ? 1 : -1) * maxDegreeYarn - halfDegreeYarn}
-			</p>
 			<div className='w-16 h-16 grid grid-cols-2 p-1 gap-1 bg-yellow-300 rounded-xl'>
 				<div className='border border-yellow-500 rounded-md' />
 				<div className='border border-yellow-500 rounded-md' />
